67096335e5ad2591c7d02b135d4a0e4ee6b7c71d
david-therincourt <david.therincourt@orange.fr>
2023-11-07 14:53:23 +0400

    supprimé :        docs/Pypi_package_build_upload.pdf 	renommé :         tests/base/csv/data_carre.csv -> examples/base/csv/data_carre.csv 	renommé :         tests/base/csv/data_integre.csv -> examples/base/csv/data_integre.csv 	renommé :         tests/base/csv/data_periode.csv -> examples/base/csv/data_periode.csv 	renommé :         tests/base/csv/ecg.csv -> examples/base/csv/ecg.csv 	renommé :         tests/base/csv/ecg2.csv -> examples/base/csv/ecg2.csv 	renommé :         tests/base/csv/ecg3.csv -> examples/base/csv/ecg3.csv 	renommé :         tests/base/derive.py -> examples/base/derive.py 	renommé :         tests/base/exemple_3.py -> examples/base/exemple_3.py 	renommé :         tests/base/integrale.py -> examples/base/integrale.py 	renommé :         tests/base/physapp -> examples/base/physapp 	renommé :         tests/base/spectre_RMS.py -> examples/base/spectre_RMS.py 	renommé :         tests/base/spectre_dB.py -> examples/base/spectre_dB.py 	renommé :         tests/csv/csv/avimeca3.txt -> examples/csv/csv/avimeca3.txt 	renommé :         tests/csv/csv/avimeca3_comma.txt -> examples/csv/csv/avimeca3_comma.txt 	renommé :         tests/csv/csv/data.txt -> examples/csv/csv/data.txt 	renommé :         tests/csv/csv/data_ltspice.txt -> examples/csv/csv/data_ltspice.txt 	renommé :         tests/csv/csv/export.txt -> examples/csv/csv/export.txt 	renommé :         tests/csv/csv/regavi.txt -> examples/csv/csv/regavi.txt 	renommé :         tests/csv/doc_exemple.py -> examples/csv/doc_exemple.py 	renommé :         tests/csv/export_csv.py -> examples/csv/export_csv.py 	renommé :         tests/csv/import_avimeca3.py -> examples/csv/import_avimeca3.py 	renommé :         tests/csv/import_avimeca3_comma.py -> examples/csv/import_avimeca3_comma.py 	renommé :         tests/csv/import_ltspice.py -> examples/csv/import_ltspice.py 	renommé :         tests/csv/import_regavi.py -> examples/csv/import_regavi.py 	renommé :         tests/csv/import_txt.py -> examples/csv/import_txt.py 	renommé :         tests/csv/physapp -> examples/csv/physapp 	renommé :         tests/curseur/curseur.py -> examples/curseur/curseur.py 	renommé :         tests/curseur/physapp -> examples/curseur/physapp 	renommé :         tests/exemples/exemple_modelisation.py -> examples/exemples/exemple_modelisation.py 	renommé :         tests/exemples/physapp -> examples/exemples/physapp 	renommé :         tests/modelisation/csv/ordre_1a.csv -> examples/modelisation/csv/ordre_1a.csv 	renommé :         tests/modelisation/csv/ordre_1b.csv -> examples/modelisation/csv/ordre_1b.csv 	renommé :         tests/modelisation/csv/ordre_2_wien.csv -> examples/modelisation/csv/ordre_2_wien.csv 	renommé :         tests/modelisation/physapp -> examples/modelisation/physapp 	renommé :         tests/modelisation/test_base.py -> examples/modelisation/test_base.py 	renommé :         tests/modelisation/test_exponentielle.py -> examples/modelisation/test_exponentielle.py 	renommé :         tests/modelisation/test_filtre.py -> examples/modelisation/test_filtre.py 	renommé :         tests/utils/data.csv -> examples/utils/data.csv 	renommé :         tests/utils/markdownListTable.py -> examples/utils/markdownListTable.py 	renommé :         tests/utils/physapp -> examples/utils/physapp 	modifié :         setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="physapp", 
-    version="0.3.2",
+    version="0.3.3",
     author="David THERINCOURT",
     author_email="dtherincourt@gmail.com",
     description="Librairie Python pour la physique appliquée",
@@ -17,6 +17,11 @@ setuptools.setup(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-    ],
+        ],
     python_requires='>=3.10',
+    install_requires=[
+        "numpy >= 1.26.0",
+        "matplotlib >= 3.8.0",
+        "scipy >= 1.11.0"
+        ]
 )
